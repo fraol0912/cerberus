58142e137fdb44b0ee1fbac2cd8f1bcfb0c1ce11
fraol0912 <fraol0912@gmail.com>
2021-06-07 02:16:00 +0300

    build: :hammer: add rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default (pkg) => {
+  return {
+    input: "src/main.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+      },
+      {
+        file: pkg.module,
+        format: "es",
+      },
+    ],
+    external: [...Object.keys(pkg.dependencies || {})],
+    plugins: [
+      typescript({
+        cacheRoot: ".rollup.cache",
+        tsconfig: "./tsconfig-build.json",
+      }),
+    ],
+  };
+};
