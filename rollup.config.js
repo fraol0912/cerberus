b1c2923bd4ecb384945df704cbd4168134c4d9ae
fraol0912 <fraol0912@gmail.com>
2021-06-07 02:57:15 +0300

    build: :hammer: add the terser plugin for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 
 export default (pkg) => {
@@ -19,6 +20,7 @@ export default (pkg) => {
         cacheRoot: ".rollup.cache",
         tsconfig: "./tsconfig-build.json",
       }),
+      terser(),
     ],
   };
 };
