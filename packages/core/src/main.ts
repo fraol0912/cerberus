189e6cb75da5242eda24868bdd5151c8a06e8a05
fraol0912 <fraol0912@gmail.com>
2021-06-09 08:33:25 +0300

    feat: :sparkles: export necessary code from main

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,1 +1,15 @@
-export const add = (x: number, y: number): number => x + y;
+// entities
+export * from "@cerberus/core/entities";
+
+// interfaces
+export * from "@cerberus/core/protocols/requests";
+export * from "@cerberus/core/protocols/presenter";
+export * from "@cerberus/core/protocols/data-access";
+
+// usecases
+export * from "@cerberus/core/usecases";
+export * from "@cerberus/core/protocols/errors";
+
+// authorizer
+export * from "@cerberus/core/main/decoder";
+export * from "@cerberus/core/main/authorizer";
