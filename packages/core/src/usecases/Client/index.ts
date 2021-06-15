d138227c0a7f6da3c81c097cd236b8a3b129b23c
fraol0912 <fraol0912@gmail.com>
2021-06-15 17:31:22 +0300

    fix: :bug: export get client usecase

--- packages/core/src/usecases/Client/index.ts
+++ packages/core/src/usecases/Client/index.ts
@@ -2,3 +2,4 @@ export * from "./CreateClientUseCase";
 export * from "./UpdateClientUseCase";
 export * from "./DeleteClientUseCase";
 export * from "./ListClientUseCase";
+export * from "./GetClientUseCase";
