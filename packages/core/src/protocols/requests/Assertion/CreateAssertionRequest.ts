ccda03179796e6864b0a201640cb40550c4722f7
fraol0912 <fraol0912@gmail.com>
2021-06-20 19:28:34 +0300

    feat: :sparkles: add name field to create assertion request

--- packages/core/src/protocols/requests/Assertion/CreateAssertionRequest.ts
+++ packages/core/src/protocols/requests/Assertion/CreateAssertionRequest.ts
@@ -2,6 +2,7 @@ import { UseCaseRequest } from "../UseCaseRequest";
 
 export interface CreateAssertionRequest extends UseCaseRequest {
   name: string;
+  subject: string;
   expiresAt: Date;
   notBefore: Date;
 }
