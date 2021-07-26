ef19cbf2459bc61f43144462047431204ba9a7d7
fraol0912 <fraol0912@gmail.com>
2021-07-26 20:49:21 +0300

    feat: :sparkles: add jwt

---
+++ packages/helpers/src/jwt/jwt.spec.ts
@@ -0,0 +1,15 @@
+import { jwt } from "./jwt";
+
+describe("Encrypt", () => {
+  it("encrypts an object", async () => {
+    const payload = {
+      random: 403,
+      iat: 1000000000,
+    };
+
+    const token = await jwt.encrypt(payload, "password");
+    const decoded = await jwt.decrypt(token, "password");
+
+    expect(decoded).toStrictEqual(payload);
+  });
+});
---
+++ packages/helpers/src/jwt/jwt.ts
@@ -0,0 +1,11 @@
+import { sign, verify } from "jsonwebtoken";
+
+export const jwt = {
+  encrypt: async (payload: object, password: string): Promise<string> => {
+    return await sign(payload, password);
+  },
+
+  decrypt: async (payload: string, password: string): Promise<any> => {
+    return (await verify(payload, password)) as object;
+  },
+};
